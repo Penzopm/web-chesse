36eef3f7441e2c3817ac3ff37aedefc757a3357b
PENZO PM <penzopmofficial@gmail.com>
2024-12-19 15:06:01 +0530

    Update webcheese.sh
    Signed-off-by: PENZO PM <penzopmofficial@gmail.com>

--- webcheese.sh
+++ webcheese.sh
@@ -4,6 +4,7 @@
 # If you use any part from this code, giving me the credits. Read the Lincense!
 
 trap 'printf "\n";stop' 2
+
 banner() {
 
 printf "\e[1;92m               _          _                          \e[0m\n"
